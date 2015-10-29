de6bb889f6adc02f88eced9798739c39ebaca409
Guilherme Silva <gg-silva@outlook.com>
2015-10-28 22:33:04 -0200

    Add class tests git

---
+++ src/EntityJokeTests/Connection/GitTesteClassTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityJokeTests.Connection
+{
+    [TestFixture]
+    public class GitTesteClassTest
+    {
+        [Test]
+        public void Teste()
+        {
+
+        }
+    }
+}
