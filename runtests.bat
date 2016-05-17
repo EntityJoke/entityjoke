612c781141119f28459f84e128264b6b1c6b1a72
Guilherme Gabriel da Silva <ggsilva@users.noreply.github.com>
2016-05-17 09:48:01 -0300

    #10 Criar package Nuget
    - Editado arquivo bat

--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,32 @@
-nunit-console.exe .\EntityJokeTests.dll
+Write-Host ""
+Write-Host ""
+Write-Host "====== [Tests] Extract files ======"
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace("C:\projects\entityjoke\test-coverage.zip")
+foreach($item in $zip.items())
+{
+  $shell.Namespace("C:\projects\entityjoke\").copyhere($item)
+}
+
+Write-Host "Extract files... Ok"
+Write-Host ""
+
+Write-Host "=========== [Tests] Run ==========="
+.\test-coverage\OpenCover\OpenCover.Console.exe -target:nunit-console.exe "-targetargs:"".\src\EntityJokeTests\bin\$env:CONFIGURATION\EntityJokeTests.dll"" /noshadow" -register:user -output:.\test-coverage\opencoverCoverage.xml
+Write-Host ""
+
+Write-Host "======== [Tests] Reporter ========="
+.\test-coverage\ReportGenerator\ReportGenerator.exe -reports:.\test-coverage\opencoverCoverage.xml -targetdir:coverage -verbosity:Info 
+
+Write-Host ""
+
+Write-Host "==== [Tests] Publish Coverage ====="
+
+$coveralls = (Resolve-Path ".\test-coverage\Coveralls\csmacnz.Coveralls.exe").ToString()
+    
+& $coveralls --opencover -i .\test-coverage\opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_VERSION --serviceName appveyor
+
+Write-Host "Publish Coverage... Ok"
+Write-Host ""
+
+Write-Host "Tests, Coverage and Reporter... Ok"
