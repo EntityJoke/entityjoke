3f3f93ba7154611204b37527f91f2f4d8175f2e7
Guilherme Gabriel da  Silva <gg-silva@outlook.com>
2016-05-20 18:32:50 -0300

    # 9 Insert Entidades com Relacionamento 1N
    - Add file ps1

---
+++ restore.ps1
@@ -0,0 +1,5 @@
+﻿Write "`n========= [Build] Install ========="
+Write "Installing Nuget Packages of EntityJoke.sln"
+nuget restore .\src\EntityJoke.sln -Verbosity Quiet
+Write "Completed installation of Nuget Packages`n"
+Write ""
