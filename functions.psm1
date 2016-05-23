f80e69edc6b33cc11cc338bc87f781f722e1129d
Guilherme Gabriel da  Silva <gg-silva@outlook.com>
2016-05-23 11:49:57 -0300

    Edited functions.psm1

--- functions.psm1
+++ functions.psm1
@@ -49,7 +49,8 @@ function RevertCommit(){
     git status
     git reset HEAD --hard
     rm -r .\coverage
-    rm -r .\teste-coverage
+    rm -r .\test-coverage
+    git reset HEAD --hard
     git status
     git remote show origin
     git revert 8d7bbd2225131c36ab354f9d31cf1c4f4a947b94 --no-edit
